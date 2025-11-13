da92546a64d7d5c52f51a150a6ce373e0457f287
Krzysztof Perdek <22326161+kperdek8@users.noreply.github.com>
2025-11-13 21:28:22 +0100

    Multi bit flip

--- src/pop_mutation.cpp
+++ src/pop_mutation.cpp
@@ -1,17 +1,19 @@
 #include "pop_mutation.hpp"
+
 #include "rng.hpp"
 #include <stdexcept>
 
-int do_bit_flip(const int chrom, const unsigned int chrom_length);
-int do_multi_bit_flip(const int chrom, const unsigned int chrom_length);
+int do_bit_flip(int chrom, unsigned int chrom_length);
+int do_multi_bit_flip(int chrom, unsigned int chrom_length, float per_bit_chance);
 
 // Punkt mutacji liczony od prawej z indeksowanem od zera
-int mutate(const int chrom, const unsigned int chrom_length, MutationMethod method) {
+// Domyslna wartosc per_bit_chance = -1.0 ma na celu wywolanie bledu podczas nie ustawienia tego parametru przy metodzie multi_bit_fip
+int mutate(const int chrom, const unsigned int chrom_length, const MutationMethod method, const float per_bit_chance = -1.0) {
     switch (method) {
         case MutationMethod::BIT_FLIP:
         return do_bit_flip(chrom, chrom_length);
         case MutationMethod::MULTI_BIT_FLIP:
-        return do_multi_bit_flip(chrom, chrom_length);
+        return do_multi_bit_flip(chrom, chrom_length, per_bit_chance);
     }
 }
 
@@ -22,6 +24,14 @@ int do_bit_flip(const int chrom, const unsigned int chrom_length) {
     return chrom ^ mutation_mask;
 }
 
-int do_multi_bit_flip(const int chrom, const unsigned int chrom_length) {
-    throw std::logic_error("Metoda mutacji MULTI_BIT_FLIP nie jest jeszcze zaimplementowana");
+int do_multi_bit_flip(int chrom, const unsigned int chrom_length, const float per_bit_chance) {
+    if (per_bit_chance <= 0.0f || per_bit_chance >= 1.0f)
+        throw std::invalid_argument("per_bit_chance musi sie miescic w <0, 1>");
+
+    for(int i = 0; i<chrom_length; ++i) {
+        if(random_float(0.0, 1.0) <= per_bit_chance) {
+            chrom ^= (1 << i);
+        }
+    }
+    return chrom;
 }
